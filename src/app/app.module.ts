0b616766c5efca8d7581c39512ee80773a19b876
neshant <nesh2391@gmail.com>
2021-01-21 17:40:36 -0600

    initial modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoriesComponent } from './stories/stories.component';
+import { BacklogComponent } from './backlog/backlog.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoriesComponent,
+    BacklogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
